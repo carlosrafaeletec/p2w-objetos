ddff75bb22f629d0cccddaf61addef8daeaf04eb
carlosrafaelgit@gmail.com <email@test.com>
2023-03-07 08:54:50 -0300

    Criacao de classe Anime

--- src/Anime.java
+++ src/Anime.java
@@ -1,3 +1,12 @@
-public class Anime {
+import java.time.LocalDate;
 
+public class Anime {
+    public int id;
+    public String nome;
+    public LocalDate data_Lançamento;
+    public String classificacaoindicativa;
+    public String genero;
+    public String autor;
+    public int qtdeEpisodios;
+    public int temporadas;
 }
---
+++ src/animeTeste.java
@@ -0,0 +1,16 @@
+public class animeTeste {
+    public static void main(String[] args) {
+        // Classe nomeObjeto = new Classe();
+        Anime meuAnime = new Anime();
+        meuAnime.nome = "Super11";
+        meuAnime.qtdeEpisodios = 32;
+        meuAnime.temporadas = 1;
+
+        Anime meuAnimePreferido = new Anime();
+        meuAnimePreferido.nome = "Naruto";
+        meuAnimePreferido.qtdeEpisodios = 327;
+        meuAnimePreferido.temporadas = 5;
+
+
+    }
+}
